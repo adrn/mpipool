90adfdca600daf0d5957af3569974efc03358d17
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-06-03 16:25:41 -0400

    added a setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from __future__ import division, print_function
+
+# Standard library
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+
+setup(
+    name="emcee",
+    version="0.0.1",
+    author="Daniel Foreman-Mackey",
+    author_email="danfm@nyu.edu",
+    packages=["mpipool"],
+    url="https://github.com/dfm/mpipool/",
+    license="MIT",
+    description="MPI pool",
+    package_data={"": ["LICENSE", "AUTHORS"]},
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+)
