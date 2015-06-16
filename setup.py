5f09ba56663dce602e7691f42e4c740840be8737
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-06-16 17:39:06 -0400

    change author and email

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ except ImportError:
 setup(
     name="mpipool",
     version="0.0.1",
-    author="Daniel Foreman-Mackey",
-    author_email="danfm@nyu.edu",
+    author="Adrian Price-Whelan",
+    author_email="adrn@astro.columbia.edu",
     packages=["mpipool"],
-    url="https://github.com/dfm/mpipool/",
+    url="https://github.com/adrn/mpipool/",
     license="MIT",
     description="MPI pool",
     package_data={"": ["LICENSE", "AUTHORS"]},
